963c0bb148cb42df736a17edf9b4fcd5f29e6c73
unknown <david.aboagye@amalitech.org>
2022-05-10 11:26:14 +0000

    working on addLecturers' component

---
+++ src/components/addLecturer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const AddLecturer = () => {
+  return (
+    <div className='lectureMain'>
+        <div className='navPane'>
+            <nav>
+
+            </nav>
+            </div>
+        <div className='addLecturerPane'>
+            <h3 id='lecturerTitle'>Add A Lecturer</h3>
+            <p id='lecturerFill'>Fill in the data for a lecturer.</p>
+        </div>
+    </div>
+  )
+}
+
+export default AddLecturer
